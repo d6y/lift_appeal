a1d6328542d395a412035d0d4bd2db79581e58ed
Richard Dallaway <richard@dallaway.com>
2013-06-30 18:00:29 +0100

    Friendlier code example.

--- src/main/scala/code/rest/PoemResource.scala
+++ src/main/scala/code/rest/PoemResource.scala
@@ -2,6 +2,7 @@ package code.rest
 
 import net.liftweb.http.rest.RestHelper
 import net.liftweb.json.JsonDSL._
+import net.liftweb.json.JsonAST._
 
 object PoemResource extends RestHelper {
 
@@ -9,9 +10,20 @@ object PoemResource extends RestHelper {
     "Larkin" -> List("This Be The Verse", "Aubade")
   )
 
+  def asJSON(titles: List[String]) : JValue =
+    ("titles" -> titles)
+
   serve {
+    case "poems" :: "by" :: author :: Nil Get request =>
+      poems.get(author).map(asJSON)
+  }
+
+}
+
+/* I might be just as likely to write this as:
+
     case "poems" :: "by" :: author :: Nil Get request =>
       for ( titles <- poems.get(author) )
         yield ("titles" -> titles) ~ ("author" -> author)
-  }
-}
+
+ */
