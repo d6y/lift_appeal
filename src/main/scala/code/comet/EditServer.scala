5c0c26dba7fb291e2337361223e3d212b67e41df
Richard Dallaway <richard@dallaway.com>
2013-07-16 22:36:00 +0100

    Removed case classes and just pass JSON around to simplify example.

--- src/main/scala/code/comet/EditServer.scala
+++ src/main/scala/code/comet/EditServer.scala
@@ -2,17 +2,18 @@ package code.comet
 
 import net.liftweb.actor.LiftActor
 import net.liftweb.http.ListenerManager
+import net.liftweb.json.JsonAST.JValue
 
 object EditServer extends LiftActor with ListenerManager {
 
   // All the edits so far:
-  var history = Vector[CharAndPos]()
+  var history = Vector[JValue]()
 
   // Handle messages from comet actors:
   override def lowPriority = {
 
     // When we receive an edit, save it in the history and pass it on to all browsers:
-    case change : CharAndPos =>
+    case change : JValue =>
       history = history :+ change
       updateListeners(change)
   }
--- src/main/scala/code/comet/Editor.scala
+++ src/main/scala/code/comet/Editor.scala
@@ -1,44 +1,37 @@
 package code.comet
 
-import net.liftweb.util.Helpers._
 import net.liftweb.http.{SHtml, CometListener, CometActor}
-
 import net.liftweb.http.js.JsCmds._
 import net.liftweb.http.js.JsCmd
 import net.liftweb.http.js.JE._
 import net.liftweb.json.JsonAST.JValue
 
-case class CharAndPos(sender: String, pos: Int, char: String) {
-  def toCmd = Call("insertAt", sender, pos, char).cmd
-}
 
 class Editor extends CometActor with CometListener {
 
   // The server code we send messages to, and receive messages from:
   def registerWith = EditServer
 
-  // When shown, bind keypresses to server events:
+  // When shown, bind key presses to server events:
   def render =
-    "#poem [onkeypress]" #> SHtml.jsonCall(Call("charAndPos(arguments)"), updateServer _)
-
+    "#poem [onkeypress]" #> SHtml.jsonCall(Call("charAndPos(arguments)"), updateServer _) &
+    "#poem [onkeyup]" #> SHtml.jsonCall(Call("detectDelete(arguments)"), updateServer _)
 
   // When we get a key event, notify the server:
-  implicit val defaults = net.liftweb.json.DefaultFormats
   def updateServer(json: JValue) : JsCmd = {
-    for (update <- json.extractOpt[CharAndPos])
-      EditServer ! update
+    EditServer ! json
     Noop
   }
 
   // When we receive an event from the server, update our browser display:
   override def lowPriority = {
 
-    // Process an edit as a JavaScript Call:
-    case edit : CharAndPos => partialUpdate(edit.toCmd)
+    // Process an edit: send the change via a JavaScript Call:
+    case edit : JValue => partialUpdate(Call("accept", edit))
 
     // If we've joined late, apply all the changes so far:
-    case edits: Vector[CharAndPos] =>
-      val js = edits.map(_.toCmd).foldLeft(Noop) { (z,edit) => z & edit }
+    case edits: Vector[JValue] =>
+      val js = edits.foldLeft(Noop) { (z,edit) => z & Call("accept",edit) }
       partialUpdate(js)
   }
 
